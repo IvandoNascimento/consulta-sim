b341c8cb6daa25845a896cfcfd4ad2946d035835
Ivan do Nascimento <nascimentodoivan@gmail.com>
2022-08-12 01:19:24 -0300

    Added some validates and associations to Paciente

--- app/models/paciente.rb
+++ app/models/paciente.rb
@@ -1,2 +1,10 @@
+
+
 class Paciente < ApplicationRecord
+  has_one :address
+  validates :name, presence:true, length: {minimum: 5}
+  validates :email, presence: true, lenght: {minimum: 5}
+  validates :cpf, presence: true, uniqueness: true
+
+  end
 end
