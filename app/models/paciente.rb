8cc9effb244ca8b689c0a58995f308515fc16182
Ivan do Nascimento <nascimentodoivan@gmail.com>
2022-08-14 15:32:09 -0300

    Allowed to destroy by associations

--- app/models/paciente.rb
+++ app/models/paciente.rb
@@ -1,8 +1,8 @@
 class Paciente < ApplicationRecord
-  has_one :address
+  has_one :address, dependent: :destroy
   validates :name, presence:true, length: {minimum: 5}
-  validates :email, presence: true, lenght: {minimum: 5}
+  validates :email, presence: true, length: {minimum: 5}
   validates :cpf, presence: true, uniqueness: true
 
-  accepts_nested_attributes_for :address
+  accepts_nested_attributes_for :address, allow_destroy: true
 end
