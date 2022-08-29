cb4f50fd762a749dde7a33baddebafde3a8b4c7c
Ivan do Nascimento <nascimentodoivan@gmail.com>
2022-08-29 09:34:08 -0300

    Added doctor tests

--- test/models/doctor_test.rb
+++ test/models/doctor_test.rb
@@ -1,7 +1,14 @@
 require "test_helper"
 
 class DoctorTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'criando um medico' do
+    medico = Doctor.new name:"Ivan do", crm: "9999999", cpf:"99999999999" , email: "teste@test.com", specialty: "Cardiologia"
+    medico.save
+  end
+  test "criando um medico sem cpf" do
+    numero_medico = Medico.all.length
+    medico = Doctor.new name:"Ivan do", crm: "9999999" , email: "teste@test.com", specialty: "Cardiologia"
+    assert medico.save
+    assert(Medico.all.length == numero_medico+1)
+  end
 end
