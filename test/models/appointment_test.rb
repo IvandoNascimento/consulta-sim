278a143d0cdcd0a9400386e07646417c90df6d3f
Ivan do Nascimento <nascimentodoivan@gmail.com>
2022-08-29 09:34:40 -0300

    Added appointment tests

--- test/models/appointment_test.rb
+++ test/models/appointment_test.rb
@@ -1,7 +1,13 @@
 require "test_helper"
 
 class AppointmentTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'criando uma consulta' do
+    consulta = Appointment.new date: "30/08/2022" ,time: "18", doctor_id: "1", paciente_id: "1"
+    assert consulta.save
+  end
+
+  test 'criando uma consulta sem medico e paciente' do
+    consulta = Appointment.new date: "30/08/2022" ,time: "18"
+    assert consulta.save
+  end
 end
