63e55025f4c72b70f8829b5e01d1470bf261ce97
Ivan do Nascimento <nascimentodoivan@gmail.com>
2022-08-29 09:33:43 -0300

    Added paciente tests

--- test/models/paciente_test.rb
+++ test/models/paciente_test.rb
@@ -1,7 +1,14 @@
 require "test_helper"
 
 class PacienteTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test 'criando um paciente completo' do
+    paciente = Paciente.new name: "Ivan do Nascimento", birthday: "04/03/2001", cpf:"99999999999" , email: "teste@test.com"
+    assert paciente.save
+  end
+
+  test 'criando um paciente sem cpf' do
+    paciente = Paciente.new name: "Ivan do Nascimento", birthday: "04/03/2001", email: "teste@test.com"
+    assert_not paciente
+  end
 end
